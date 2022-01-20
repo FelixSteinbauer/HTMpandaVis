6e5d0228d69309795072338dfd1547765b61afff
FelixSteinbauer <felix.steinbauer@tum.de>
2022-01-20 14:55:02 +0100

    Setup file for bakeReader added

---
+++ PandaVis/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(name='bakeReader',
+      version='0.1',
+      packages = ["bakeReader"],
+      author="Zbysek Zapadlik from HTM community",
+      url="https://github.com/htm-community/HTMpandaVis",
+      python_requires='>=3.6',
+      )
